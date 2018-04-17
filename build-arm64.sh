161292c18bd727916337848fdd958985b1388a4b
xiongyingqi <xiongyingqi@xunlei.com>
2018-04-17 10:24:22 +0800

    add login

--- build-arm64.sh
+++ build-arm64.sh
@@ -6,7 +6,11 @@ cp web-crawler.go web-crawler.yaml web-crawler-docker work-dir/
 #docker run --rm -v `pwd`/work-dir/:/work-dir:rw -it golang go get gopkg.in/yaml.v2 ; env GOOS=linux GOARCH=arm64 go build -o /work-dir/web-crawler /work-dir/web-crawler.go
 #docker run --rm -v `pwd`/work-dir/:/work-dir:rw -it golang "go get gopkg.in/yaml.v2"
 cd work-dir
-env GOOS=linux GOARCH=arm go build -o web-crawler web-crawler.go
-docker build -f web-crawler-docker -t web-crawler:v1.0.0 .
+env GOOS=linux GOARCH=arm64 go build -o web-crawler web-crawler.go
+version="v`date  +"%Y%m%d%H%M%s"`"
+tag="hub.xycloud.com/4164/web-crawler:${version}"
+docker build -f web-crawler-docker -t $tag .
+docker login hub.xycloud.com -u 15084818017 -p xl606017
+docker push $tag
 cd "$cur_dir"
-#rm -fr work-dir
+rm -fr work-dir
