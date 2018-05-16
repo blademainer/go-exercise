3da95c222070e45ae311f7143cc664653b46f8c7
xiongyingqi <xiongyingqi@xunlei.com>
2018-05-16 14:32:23 +0800

    struct cache

--- struct_pointer.go
+++ struct_pointer.go
@@ -4,7 +4,6 @@ import (
 	"sync/atomic"
 	"sync"
 	"fmt"
-	"reflect"
 )
 
 type P struct {
@@ -18,46 +17,27 @@ type fieldCache struct {
 	mu    sync.Mutex   // used only by writers
 }
 
-
-
 func (p *P) Process(){
-	fmt.Printf("%p \n", p)
-	printAndReadFields(*p, *p)
+	fmt.Printf("Process &p: %p \n", p)
+	printAndReadFields(*p)
 }
 
-func printAndReadFields(p P, entity interface{}) {
-	fmt.Printf("%p \n", &p)
+func printAndReadFields(p P) {
+	fmt.Printf("printAndReadFields &p: %p \n", &p)
 
-	fields := (&p).cachedTypeFields(reflect.ValueOf(entity))
-	fmt.Println("fields: ", fields)
-}
-
-type field struct {
-	name string
-	typ reflect.Value
-}
-
-func (p *P) cachedTypeFields(t reflect.Value) []field {
-	fmt.Printf("%p \n", p)
-	cache := p.cache.value.Load().(map[reflect.Type][]field)
-	fields := cache[t.Type()]
-	if fields != nil {
-		return fields
+	if p.cache == nil {
+		fmt.Println("init cache...")
+		p.cache = &fieldCache{}
 	}
-	readFields := readFields(t.Type())
-	p.cache.mu.Lock()
-	p.cache.value.Store(readFields)
-	p.cache.mu.Unlock()
-	return readFields
+	// update cache...
 }
 
-func readFields(t reflect.Type) []field {
-	return nil
-}
 
 func main() {
 	p := &P{}
 	p.Name = "张三"
 	p.Age = 18
+	p.Process()
+	p.Process()
 	p.Process()
 }
