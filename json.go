1ead278a29057ecdc20f6370824be833b758cac5
xiongyingqi <xiongyingqi@xunlei.com>
2018-05-23 11:43:47 +0800

    pretty json

--- json.go
+++ json.go
@@ -5,23 +5,32 @@ import (
 	"fmt"
 )
 
-type Student struct {
-	Age  uint   `json:"age"`
-	Name string `json:"name"`
-}
-
-func (student *Student) String() string {
-	bytes, _ := json.Marshal(student)
-	return string(bytes)
-}
-
 func main() {
-	student := Student{Age: 18, Name: "zhang san"}
+	type Person struct {
+		Age    uint    `json:"age"`
+		Name   string  `json:"name"`
+		Parent *Person `json:"parent"`
+	}
+
+	student := Person{Age: 18, Name: "zhang san"}
+	student.Parent = &Person{Age: 28, Name: "zhang er"}
 	bytes, _ := json.Marshal(student)
 	fmt.Println(string(bytes))
 
-	target := &Student{}
+	// pretty json
+	fmt.Println(string(PrettyJson(bytes)))
+
+	target := &Person{}
 	json.Unmarshal(bytes, target)
 	fmt.Printf("%s \n", target)
 	fmt.Println(*target == student)
+}
+
+func PrettyJson(bytes []byte) []byte {
+	strings := make(map[string]interface{})
+	ptr := &strings
+	json.Unmarshal(bytes, ptr)
+	fmt.Println("map: ", ptr)
+	pretty, _ := json.MarshalIndent(ptr, "", "    ")
+	return pretty
 }
