f4b3bb79e10bf6bd5999e4dfe5b8fce80a1e3067
xiongyingqi <xiongyingqi@xunlei.com>
2018-05-29 16:45:04 +0800

    pointer

--- pointer.go
+++ pointer.go
@@ -7,5 +7,18 @@ func main() {
 	b := "world"
 	*(&a), *(&b) = b, a
 
-	fmt.Printf("a=%v, b=%v", a, b)
+	fmt.Printf("a=%v, b=%v \n", a, b)
+	c := &a
+	d := &c
+
+	fmt.Println("aaa")
+	fmt.Printf("Type=%T \n", d)
+}
+
+func pp(in interface{}) interface{} {
+	for i := 0; i <= 100; i++ {
+		in = &in
+		fmt.Printf("Type=%T \n", in)
+	}
+	return in
 }
