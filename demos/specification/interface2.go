1cf18079b220dad198ecee54cc469fd883193daa
blademainer <blademainer@gmail.com>
2018-06-26 00:40:17 +0800

    add slide

---
+++ demos/specification/interface2.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type I2 interface {
+	Set(int)
+}
+
+type T2 struct {
+	i int
+}
+
+func (t2 *T2) Set(i int) {
+	t2.i = i
+}
+
+func f(i I2){
+	i.Set(10)
+}
+
+func main() {
+	t2 := &T2{}
+	f(t2)
+	fmt.Println(t2.i)
+}
