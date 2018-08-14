f3959571064a32d0731366e259d3a06d3be20aa3
xiongyingqi <xiongyingqi@xunlei.com>
2018-08-14 15:21:43 +0800

    add defer

--- demos/channel/multi_goroutine.go
+++ demos/channel/multi_goroutine.go
@@ -10,20 +10,23 @@ func main() {
 	wg := sync.WaitGroup{}
 	wg.Add(2)
 
-	dataCh := make(chan int, 1024)
+	dataCh := make(chan int, 1000)
 	closeCh := make(chan bool, 2)
 	//doneCh := make(chan bool, 1024)
 	go func() {
 		for i := 0; i < 10000; i++ {
 			dataCh <- i
 		}
+		fmt.Println("Done...")
 		closeCh <- true
 		closeCh <- true
 		//wg.Done()
+		//close(dataCh)
 	}()
 
 	count := int32(0)
 	go func() {
+		defer wg.Done()
 		fmt.Println("Starting...")
 		for {
 			select {
@@ -31,13 +34,19 @@ func main() {
 				fmt.Printf("Receive i：%v \n", s)
 				atomic.AddInt32(&count, 1)
 			case <-closeCh:
-				fmt.Printf("Closing...")
-				wg.Done()
+				fmt.Printf("Closing... cur length: %d \n", len(dataCh))
 				return
 			}
 		}
 	}()
 	go func() {
+		defer wg.Done()
+		defer func() {
+			for len(dataCh) > 0{
+				fmt.Printf("Read least data: %d \n", <-dataCh)
+				atomic.AddInt32(&count, 1)
+			}
+		}()
 		fmt.Println("Starting...")
 		for {
 			select {
@@ -45,8 +54,7 @@ func main() {
 				fmt.Printf("Receive i：%v \n", s)
 				atomic.AddInt32(&count, 1)
 			case <-closeCh:
-				fmt.Printf("Closing...")
-				wg.Done()
+				fmt.Printf("Closing... cur length: %d \n", len(dataCh))
 				return
 			}
 		}
