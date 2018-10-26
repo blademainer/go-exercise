5f55ff0c86a13ab8e16576a5b7baa9c4887b77a5
blademainer <blademainer@gmail.com>
2018-10-26 18:02:17 +0800

    update panic

--- demos/http/nil_response.go
+++ demos/http/nil_response.go
@@ -3,17 +3,27 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
-	resp, err := http.DefaultClient.Get("http://unkonwn")
-	if resp != nil && resp.Body != nil{
-		defer resp.Body.Close()
-	}
-	if err != nil{
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(resp)
-
+	go func() {
+		//defer func() {
+		//	if i := recover(); i != nil{
+		//		fmt.Println("Panic with: ", i)
+		//	}
+		//}()
+		resp, err := http.DefaultClient.Get("http://unkonwn")
+		resp.Body.Close()
+		//if resp != nil && resp.Body != nil{
+		//	defer resp.Body.Close()
+		//}
+		if err != nil{
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(resp)
+	}()
+	time.Sleep(2 * time.Second)
+	fmt.Println("aaaa")
 }
