336133c2b4ffc064645fda51439e48d717c57d77
blademainer <blademainer@gmail.com>
2019-10-28 11:27:57 +0800

    update keystore

---
+++ demos/errshadow/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+import "strconv"
+
+func foo(x string) (ret int, err error) {
+	if true {
+		_, err := strconv.Atoi(x)
+		if err != nil {
+			// should compile error
+			return
+		}
+	}
+	return ret, nil
+}
+
+func main() {
+	fmt.Println(foo("123"))
+}
