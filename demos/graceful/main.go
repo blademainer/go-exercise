cb2e8a1b8a59f19c84a1332f32e45466a1946835
blademainer <blademainer@gmail.com>
2020-04-03 17:37:24 +0800

    add graceful demo

---
+++ demos/graceful/main.go
@@ -0,0 +1,179 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	_ "net/http/pprof"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/blademainer/commons/pkg/logger"
+
+	"golang.org/x/sync/errgroup"
+	"google.golang.org/grpc"
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"
+	"google.golang.org/grpc/health"
+	healthpb "google.golang.org/grpc/health/grpc_health_v1"
+	"google.golang.org/grpc/keepalive"
+)
+
+const serviceName = "myserviced"
+
+var (
+	version = "no version"
+
+	debugPort  = flag.Int("debugPort", 16161, "debug port")
+	httpPort   = flag.Int("httpPort", 8888, "http port")
+	grpcPort   = flag.Int("grpcPort", 9200, "grpc port")
+	healthPort = flag.Int("healthPort", 6666, "grpc health port")
+)
+
+type server struct {
+}
+
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
+
+func main() {
+	flag.Parse()
+
+	logger.Infof("Starting app, version: %v", version)
+
+	// shutdown functions
+	shutdownFunctions := make([]func(), 0)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	shutdownFunctions = append(shutdownFunctions, cancel)
+	defer cancel()
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+	defer signal.Stop(interrupt)
+
+	g, ctx := errgroup.WithContext(ctx)
+
+	g.Go(func() error {
+		//profiles := pprof.Profiles()
+
+		httpServer := &http.Server{
+			Addr:         fmt.Sprintf(":%d", *debugPort),
+			ReadTimeout:  10 * time.Second,
+			WriteTimeout: 10 * time.Second,
+			Handler:      nil,
+		}
+		shutdownFunctions = append(shutdownFunctions, func() {
+			err := httpServer.Shutdown(ctx)
+			if err != nil {
+				logger.Errorf("failed to shutdown pprof server! error: %v", err.Error())
+			}
+		})
+
+		logger.Infof("pprof server serving at :%d", *debugPort)
+
+		if err := httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			logger.Errorf("failed to listen: %v", err.Error())
+			return err
+		}
+		return nil
+	})
+
+	// web server metrics
+	g.Go(func() error {
+		httpServer := &http.Server{
+			Addr:         fmt.Sprintf(":%d", *httpPort),
+			ReadTimeout:  10 * time.Second,
+			WriteTimeout: 10 * time.Second,
+		}
+		shutdownFunctions = append(shutdownFunctions, func() {
+			err := httpServer.Shutdown(ctx)
+			if err != nil {
+				logger.Errorf("failed to shutdown pprof server! error: %v", err.Error())
+			}
+		})
+		logger.Infof("HTTP Metrics server serving at :%d", *httpPort)
+
+		if err := httpServer.ListenAndServe(); err != http.ErrServerClosed {
+			return err
+		}
+
+		return nil
+	})
+
+	// gRPC Health Server
+	healthServer := health.NewServer()
+	g.Go(func() error {
+		grpcHealthServer := grpc.NewServer()
+
+		shutdownFunctions = append(shutdownFunctions, func() {
+			healthServer.SetServingStatus(fmt.Sprintf("grpc.health.v1.%s", serviceName), healthpb.HealthCheckResponse_NOT_SERVING)
+			grpcHealthServer.GracefulStop()
+		})
+
+		healthpb.RegisterHealthServer(grpcHealthServer, healthServer)
+
+		haddr := fmt.Sprintf(":%d", *healthPort)
+		hln, err := net.Listen("tcp", haddr)
+		if err != nil {
+			logger.Errorf("gRPC Health server: failed to listen, error: %v", err)
+			os.Exit(2)
+		}
+		logger.Infof("gRPC health server serving at %s", haddr)
+		return grpcHealthServer.Serve(hln)
+	})
+
+	// gRPC server
+	g.Go(func() error {
+		addr := fmt.Sprintf(":%d", *grpcPort)
+		ln, err := net.Listen("tcp", addr)
+		if err != nil {
+			logger.Errorf("gRPC server: failed to listen, error: %v", err)
+			os.Exit(2)
+		}
+
+		server := &server{
+		}
+		grpcServer := grpc.NewServer(
+			// MaxConnectionAge is just to avoid long connection, to facilitate load balancing
+			// MaxConnectionAgeGrace will torn them, default to infinity
+			grpc.KeepaliveParams(keepalive.ServerParameters{MaxConnectionAge: 2 * time.Minute}),
+		)
+		pb.RegisterGreeterServer(grpcServer, server)
+		shutdownFunctions = append(shutdownFunctions, func() {
+			healthServer.SetServingStatus(fmt.Sprintf("grpc.health.v1.%s", serviceName), healthpb.HealthCheckResponse_NOT_SERVING)
+			grpcServer.GracefulStop()
+		})
+
+		logger.Infof("gRPC server serving at %s", addr)
+
+		healthServer.SetServingStatus(fmt.Sprintf("grpc.health.v1.%s", serviceName), healthpb.HealthCheckResponse_SERVING)
+
+		return grpcServer.Serve(ln)
+	})
+
+	select {
+	case <-interrupt:
+		break
+	case <-ctx.Done():
+		break
+	}
+
+	logger.Warnf("received shutdown signal")
+
+	for _, shutdown := range shutdownFunctions {
+		shutdown()
+	}
+
+	err := g.Wait()
+	if err != nil {
+		logger.Errorf("server returning an error, error: %v", err)
+		os.Exit(2)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,14 +1,15 @@
 module github.com/blademainer/go-exercise
 
 require (
+	cloud.google.com/go v0.26.0
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
 	github.com/alecthomas/units v0.0.0-20190924025748-f65c72e2690d // indirect
+	github.com/blademainer/commons v0.0.13
 	github.com/chrislusf/glow v0.0.0-20181102060906-4c40a2717eee
-	github.com/coreos/etcd v3.3.10+incompatible
+	github.com/coreos/etcd v3.3.13+incompatible
 	github.com/gin-contrib/sse v0.0.0-20170109093832-22d885f9ecc7 // indirect
 	github.com/gin-gonic/gin v0.0.0-20170702092826-d459835d2b07
 	github.com/golang/mock v1.3.1
-	github.com/golang/protobuf v0.0.0-20180328163153-e09c5db29600 // indirect
 	github.com/inconshreveable/go-update v0.0.0-20160112193335-8152e7eb6ccf // indirect
 	github.com/mattn/go-isatty v0.0.4 // indirect
 	github.com/psilva261/timsort v0.0.0-20190627160904-7d0a5d71371e // indirect
@@ -17,11 +18,13 @@ require (
 	github.com/weaveworks/procspy v0.0.0-20150706124340-cb970aa190c3
 	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2
 	golang.org/x/net v0.0.0-20190311183353-d8887717615a
+	golang.org/x/sync v0.0.0-20190423024810-112230192c58
 	golang.org/x/tour v0.0.0-20180314180224-daf29d8a3774
+	google.golang.org/grpc v1.26.0
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6 // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
 	gopkg.in/yaml.v2 v2.2.7
 )
 
-go 1.13
+go 1.14
