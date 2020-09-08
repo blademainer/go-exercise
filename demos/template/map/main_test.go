81d29441be037b8085eb2d273f8bb346c58ac9f2
blademainer <blademainer@gmail.com>
2020-09-08 19:53:44 +0800

    update template

--- demos/template/map/main_test.go
+++ demos/template/map/main_test.go
@@ -9,3 +9,51 @@ func BenchmarkExecute(b *testing.B) {
 	}
 }
 
+func TestExecuteData(t *testing.T) {
+	Init()
+	type args struct {
+		data interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "t1",
+			args: args{
+				data: map[string]interface{}{
+					"person": Person{"Zhangsan", 11},
+					"m": map[string]interface{}{
+						"name": "zhangsan",
+						"age":  11,
+					},
+				},
+			},
+			want: `My dear master! 
+your age: 11 multiply twice age: 22
+map age: 11`,
+		},
+		{
+			name: "t2",
+			args: args{
+				data: map[string]interface{}{
+					"person": Person{"Zhangsan", 11},
+					"m": map[string]interface{}{
+						"name": "zhangsan",
+					},
+				},
+			},
+			want: `My dear master! 
+your age: 11 multiply twice age: 22
+map age: 0`,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ExecuteData(tt.args.data); got != tt.want {
+				t.Errorf("ExecuteData() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
