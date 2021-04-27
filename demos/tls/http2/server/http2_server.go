31865a0b62eb7ec6162562ada090fa30ebfc5d39
blademainer <blademainer@gmail.com>
2021-04-27 20:53:41 +0800

    update ca scripts

--- demos/tls/http2/server/http2_server.go
+++ demos/tls/http2/server/http2_server.go
@@ -12,20 +12,6 @@ import (
 type myhandler struct {
 }
 
-func (h *myhandler) ServeHTTP(w http.ResponseWriter,
-	r *http.Request) {
-	fmt.Fprintf(w,
-		"Hi, This is an example of http service in golang!\n")
-	"crypto/tls"
-	"crypto/x509"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-)
-
-type myhandler struct {
-}
-
 func (h *myhandler) ServeHTTP(
 	w http.ResponseWriter,
 	r *http.Request,
--- demos/tls/key/gen_rsa.sh
+++ demos/tls/key/gen_rsa.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 openssl genrsa -out ca.key 2048
-openssl req -new -x509 -key ca.key -days 1095 -out ca.crt -subj "/C=CN/ST=GD/L=SZ/O=XL/OU=IT/CN=localhost/emailAddress=blademainer@gmail.com"
+openssl req -new -x509 -key ca.key -days 1095 -out ca.crt -subj "/C=CN/ST=GD/L=SZ/O=BLADEMAINER/OU=IT/CN=localhost/emailAddress=blademainer@gmail.com"
 
 openssl x509 -enddate -noout -in ca.crt
 
 openssl genrsa -out server.pem 2048
 openssl rsa -in server.pem -out server.key
-openssl req -new -key server.pem -out server.csr -subj "/C=CN/ST=GD/L=SZ/O=XL/OU=IT/CN=localhost/emailAddress=blademainer@gmail.com"
+openssl req -new -key server.pem -out server.csr -subj "/C=CN/ST=GD/L=SZ/O=BLADEMAINER/OU=IT/CN=localhost/emailAddress=blademainer@gmail.com"
 openssl x509 -req -sha256 -in server.csr -days 1095 -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt
 
 
 openssl genrsa -out client.pem 2048
 openssl rsa -in client.pem -out client.key
-openssl req -new -key client.pem -out client.csr -subj "/C=CN/ST=GD/L=SZ/O=XL/OU=IT/CN=localhost/emailAddress=blademainer@gmail.com"
+openssl req -new -key client.pem -out client.csr -subj "/C=CN/ST=GD/L=SZ/O=BLADEMAINER/OU=IT/CN=localhost/emailAddress=blademainer@gmail.com"
 openssl x509 -req -sha256 -in client.csr -days 1095 -CA ca.crt -CAkey ca.key -CAcreateserial -out client.crt
 #openssl pkcs12 -export -clcerts -in client.crt -inkey client.key -out client.pfx
 
