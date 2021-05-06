0156925d72916c517d090d64f6f9afe3035a5539
blademainer <blademainer@gmail.com>
2021-05-06 16:28:46 +0800

    update gen rsa

--- demos/tls/key/gen_rsa.sh
+++ demos/tls/key/gen_rsa.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
-openssl genrsa -out ca.key 2048
-openssl req -new -x509 -key ca.key -days 1095 -out ca.crt -subj "/C=CN/ST=GD/L=SZ/O=BLADEMAINER/OU=IT/CN=localhost/emailAddress=blademainer@gmail.com"
+cur_script_dir="$(dirname "$0")"
 
-openssl x509 -enddate -noout -in ca.crt
+openssl genrsa -out ${cur_script_dir}/ca.key 2048
+openssl req -new -x509 -key ${cur_script_dir}/ca.key -days 1095 -out ${cur_script_dir}/ca.crt -subj "/C=CN/ST=GD/L=SZ/O=BLADEMAINER/OU=IT/CN=localhost/emailAddress=blademainer@gmail.com"
 
-openssl genrsa -out server.pem 2048
-openssl rsa -in server.pem -out server.key
-openssl req -new -key server.pem -out server.csr -subj "/C=CN/ST=GD/L=SZ/O=BLADEMAINER/OU=IT/CN=localhost/emailAddress=blademainer@gmail.com"
-openssl x509 -req -sha256 -in server.csr -days 1095 -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt
+openssl x509 -enddate -noout -in ${cur_script_dir}/ca.crt
 
-
-openssl genrsa -out client.pem 2048
-openssl rsa -in client.pem -out client.key
-openssl req -new -key client.pem -out client.csr -subj "/C=CN/ST=GD/L=SZ/O=BLADEMAINER/OU=IT/CN=localhost/emailAddress=blademainer@gmail.com"
-openssl x509 -req -sha256 -in client.csr -days 1095 -CA ca.crt -CAkey ca.key -CAcreateserial -out client.crt
-#openssl pkcs12 -export -clcerts -in client.crt -inkey client.key -out client.pfx
+openssl genrsa -out ${cur_script_dir}/server.pem 2048
+openssl rsa -in ${cur_script_dir}/server.pem -out ${cur_script_dir}/server.key
+openssl req -new -key ${cur_script_dir}/server.pem -out server.csr -subj "/C=CN/ST=GD/L=SZ/O=BLADEMAINER/OU=IT/CN=localhost/emailAddress=blademainer@gmail.com"
+openssl x509 -req -sha256 -in server.csr -days 1095 -CA ${cur_script_dir}/ca.crt -CAkey ${cur_script_dir}/ca.key -CAcreateserial -out ${cur_script_dir}/server.crt
 
 
+openssl genrsa -out ${cur_script_dir}/client.pem 2048
+openssl rsa -in ${cur_script_dir}/client.pem -out ${cur_script_dir}/client.key
+openssl req -new -key ${cur_script_dir}/client.pem -out ${cur_script_dir}/client.csr -subj "/C=CN/ST=GD/L=SZ/O=BLADEMAINER/OU=IT/CN=localhost/emailAddress=blademainer@gmail.com"
+openssl x509 -req -sha256 -in ${cur_script_dir}/client.csr -days 1095 -CA ${cur_script_dir}/ca.crt -CAkey ${cur_script_dir}/ca.key -CAcreateserial -out ${cur_script_dir}/client.crt
+#openssl pkcs12 -export -clcerts -in ${cur_script_dir}/client.crt -inkey ${cur_script_dir}/client.key -out client.pfx
 
