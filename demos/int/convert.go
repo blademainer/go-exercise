e59ca6b0fc6a7c4abb2a13b8533f28f0ee77f6c9
blademainer <blademainer@gmail.com>
2021-07-01 21:01:12 +0800

    add bytes convert

--- demos/int/convert.go
+++ demos/int/convert.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"strconv"
 )
@@ -9,4 +10,6 @@ func main() {
 	fmt.Println("string cast: ", string(199))
 	fmt.Println("string cast: ", string(1))
 	fmt.Println("strconv: ", strconv.FormatInt(199, 10))
+	fmt.Println(binary.BigEndian.Uint16([]byte{1, 255}))
+	fmt.Println(binary.LittleEndian.Uint16([]byte{1, 255}))
 }
