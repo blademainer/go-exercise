08322030481654654be1c93ca45904b94959f6eb
blademainer <blademainer@gmail.com>
2022-01-07 16:35:59 +0800

    update json demo

--- demos/jsonpb/escape/main.go
+++ demos/jsonpb/escape/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 
 	"github.com/golang/protobuf/jsonpb"
@@ -8,13 +10,26 @@ import (
 )
 
 func main() {
-	ms := &jsonpb.Marshaler{
-
-	}
-	r := &helloworld.HelloRequest{Name: "zhagnsan&asd>=123"}
+	ms := &jsonpb.Marshaler{}
+	r := &helloworld.HelloRequest{Name: "zhagnsan&asd=123"}
 	str, err := ms.MarshalToString(r)
 	if err != nil {
 		panic(err.Error())
 	}
-	fmt.Println(str)
+	fmt.Println("jsonpb: ", str)
+
+	marshal, err := json.Marshal(r)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println("std json: ", string(marshal))
+
+	buf := &bytes.Buffer{}
+	encoder := json.NewEncoder(buf)
+	encoder.SetEscapeHTML(false)
+	err = encoder.Encode(r)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("disable escape json: ", buf.String())
 }
