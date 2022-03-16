5627d1806343ae36a423062b70ff4f9513ceb041
blademainer <blademainer@gmail.com>
2022-03-16 10:41:43 +0800

    add go1.18 generic

---
+++ demos/generic/container.go
@@ -0,0 +1,23 @@
+package generic
+
+type Number interface {
+	int64 | float64 | int
+}
+
+// SumIntOrFloat sum of map
+func SumIntOrFloat[K comparable, V int | float64](m map[K]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+// SumNumber sum of map
+func SumNumber[K comparable, V Number](m map[K]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
---
+++ demos/generic/container_test.go
@@ -0,0 +1,67 @@
+package generic
+
+import (
+	"testing"
+)
+
+func TestSumIntOrFloat(t *testing.T) {
+	type args struct {
+		m map[string]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "t1",
+			args: args{
+				m: map[string]int{
+					"a": 1,
+					"b": 2,
+				},
+			},
+			want: 3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(
+			tt.name, func(t *testing.T) {
+				if got := SumIntOrFloat(tt.args.m); got != tt.want {
+					t.Errorf("Sum() = %v, want %v", got, tt.want)
+				}
+			},
+		)
+	}
+}
+
+func TestSumNumber(t *testing.T) {
+	type args struct {
+		m map[string]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "t1",
+			args: args{
+				m: map[string]int{
+					"a": 1,
+					"b": 2,
+				},
+			},
+			want: 3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(
+			tt.name, func(t *testing.T) {
+				if got := SumNumber(tt.args.m); got != tt.want {
+					t.Errorf("Sum() = %v, want %v", got, tt.want)
+				}
+			},
+		)
+	}
+}
