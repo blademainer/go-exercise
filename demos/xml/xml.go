741afd4f1a48f9c206dcb80c4249fd01139359ab
blademainer <blademainer@gmail.com>
2024-02-28 20:52:07 +0800

    update xml demo

--- demos/xml/xml.go
+++ demos/xml/xml.go
@@ -1,29 +1,41 @@
 package main
 
 import (
-	"bytes"
 	"encoding/xml"
 	"fmt"
 )
 
 type XMLProduct struct {
-	XMLName          xml.Name `xml:"row"`
-	ProductId        string   `xml:"product_id"`
-	ProductName      string   `xml:"![CDATA["`
-	OriginalPrice    string   `xml:"original_price"`
-	BargainPrice     string   `xml:"bargain_price"`
-	TotalReviewCount int      `xml:"total_review_count"`
-	AverageScore     float64  `xml:"average_score"`
+	XMLName     xml.Name `xml:"optin-request"`
+	ProductId   string   `xml:"product_id"`
+	ProductName Cdata    `xml:"product_name"`
+	// ProductName      string  `xml:",cdata"`
+	OriginalPrice    string  `xml:"original_price"`
+	BargainPrice     string  `xml:"bargain_price"`
+	TotalReviewCount int     `xml:"total_review_count"`
+	AverageScore     float64 `xml:"average_score"`
+	Timeout          struct {
+		After string `xml:"after,attr"`
+	} `xml:"timeout"`
+}
+
+type Cdata struct {
+	Value string `xml:",cdata"`
 }
 
 func main() {
 	prod := XMLProduct{
 		ProductId:        "ProductId",
-		ProductName:      "ProductName",
+		ProductName:      Cdata{Value: "ProductName"},
 		OriginalPrice:    "OriginalPrice",
 		BargainPrice:     "BargainPrice",
 		TotalReviewCount: 20,
 		AverageScore:     2.1,
+		Timeout: struct {
+			After string `xml:"after,attr"`
+		}{
+			After: "10",
+		},
 	}
 
 	out, err := xml.MarshalIndent(prod, " ", "  ")
@@ -33,7 +45,7 @@ func main() {
 	}
 	fmt.Println(string(out))
 
-	out = bytes.Replace(out, []byte("<![CDATA[>"), []byte("<![CDATA["), -1)
-	out = bytes.Replace(out, []byte("</![CDATA[>"), []byte("]]>"), -1)
-	fmt.Println(string(out))
+	// out = bytes.Replace(out, []byte("<![CDATA[>"), []byte("<![CDATA["), -1)
+	// out = bytes.Replace(out, []byte("</![CDATA[>"), []byte("]]>"), -1)
+	// fmt.Println(string(out))
 }
