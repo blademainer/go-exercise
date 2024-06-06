4b2d645ebd3ac37d73b1fbd1cb28408a7546ec60
blademainer <blademainer@gmail.com>
2024-06-06 14:26:25 +0800

    update refund design

--- demos/slice/closure/closure.go
+++ demos/slice/closure/closure.go
@@ -22,17 +22,41 @@ func process() func(*sync.WaitGroup, []string) error {
 	}
 }
 
-func main() {
+func process2(strings []string) error {
 	wg := &sync.WaitGroup{}
-	f := process()
-	s := make([]string, 0)
-	for i := 0; i < 100; i++ {
-		s = make([]string, 0)
-		s = append(s, "hello"+strconv.Itoa(i))
-		err := f(wg, s)
-		if err != nil {
-			panic(err)
-		}
+	for _, s := range strings {
+		wg.Add(1)
+		go func(s string) {
+			defer wg.Done()
+			time.Sleep(1 * time.Second)
+			// loop strings
+			fmt.Println(s)
+		}(s)
 	}
 	wg.Wait()
+	return nil
+}
+
+func main() {
+	// wg := &sync.WaitGroup{}
+	// f := process()
+	// s := make([]string, 0)
+	// for i := 0; i < 100; i++ {
+	// 	s = make([]string, 0)
+	// 	s = append(s, "hello"+strconv.Itoa(i))
+	// 	err := f(wg, s)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// }
+	// wg.Wait()
+
+	s2 := make([]string, 0)
+	for i := 0; i < 100; i++ {
+		s2 = append(s2, "hello process2: "+strconv.Itoa(i))
+	}
+	err := process2(s2)
+	if err != nil {
+		panic(err)
+	}
 }
